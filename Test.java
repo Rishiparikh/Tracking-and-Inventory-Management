8af79443fcf9a1c08185b5b0e35d16d2e850b676
Rishi Parikh <rishiparikh2001@gmail.com>
2018-05-07 12:22:26 -0700

    Update Test.java

--- Test.java
+++ Test.java
@@ -1,12 +1,16 @@
 import java.util.ArrayList;
 public class Test{
   public static void main(String[] args){
+    map = new SketchPad(1000,1000);
+    pen = new DrawingTOol(map);
+    GUI g = new GUI(map, pen);
     ArrayList<Item> a = new ArrayList<Item>();
     a.add(new Item("food", 50, 0));
     a.add(new Item("water", 50, 0));
     a.add(new Item("stuff", 30, 0));
-    Storage b = new Storage(a, 0, 0, 40, 45, 0);
-    Storage c = new Storage(a, 0, 0, 40, 45, 0);
-    System.out.println(b.checkRedAlert());
+    Storage b = new Storage(a, 0, 0, 40, 45, 100);
+    Storage c = new Storage(a, 150, 150, 40, 45, 100);
+    g.draw(b);
+    g.draw(c);
   }
 }
