d8eabcdf647dde4c075b4ab0bb14b9da546da2f8
shaheryar6131 <38920084+shaheryar6131@users.noreply.github.com>
2018-05-09 07:43:54 -0700

    Update WareHouse.java

--- WareHouse.java
+++ WareHouse.java
@@ -18,52 +18,68 @@ public class WareHouse extends Storage implements Shippable{
     }
     return false;
   }
+  
+  public void refillHub(int i)
+  {
+	  //////////////////////// this method is incomplete it will check which items needs to refilled in hubs and refil them
+	  hubs.get(i)......
+  }
+  // returns : 0 for normal status, 1 : for yellow flag and 2 : for red flag
+  public int hasUpdate(int i)
+  {
+	    if(hubs.get(i).checkRedAlert() >= 0 )
+	    	return 2;
+	    else if(hubs.get(i).checkYellowAlert() >= 0 )
+	    	return 1;
+	    else return 0;
+  }
 
   public boolean shipItem(String name, int amount, int xCord, int yCord)
   {
-	  int nearest = 0;
-	  int distance=0;
+	  int nearest = -1;
+	  int distance=-1;
+	  
+	  // first check if the warehouse has the item sotred and what is the distance
+	  
+	  int [] wareLoc=super.getLocation();
+	  int wareHouse_distance = (wareLoc[0]-xCord)* (wareLoc[0]-xCord) + (wareLoc[1]-yCord)* (wareLoc[1]-yCord);
+	  if(super.hasItemQuantity())
+	  {
+		  nearest=-2;
+		  distance=wareHouse_distance;
+	  }
 	  for (int i = 0; i < hubs.size(); i++) 
 	  {
 		  int [] loc = hubs.get(i).getLocation();
 		  int temp_distance = (loc[0]-xCord)* (loc[0]-xCord) + (loc[1]-yCord)* (loc[1]-yCord);
-		  if(i==0)
-			  distance=temp_distance;
-		  else
+		  if( hubs.get(i).hasItemQuantity(name,amount))
 		  {
-			  
-		  }
-		  if(itemName.equals(name))
-		  {
-			  int availableQuantity =  myInv.get(i).getQuantity();
-			  if(availableQuantity>=amount)
+			  if(nearest==-1)
 			  {
-				  myInv.get(i).updateQuantity(amount);
-				  
-				 int rflag= checkRedAlert();
-				 if(rflag > 0)
-				   		redAlert=true;
-				 else
-					 redAlert=false;
-					 
-				if(redAlert)	 
-					yellowAlert=true;
-				else
-				{
-					 int yflag= checkYellowAlert();
-					 if(yflag > 0)
-					   		yellowAlert=true;
-					 else
-						 yellowAlert=false;
-				}
-				 
+			  distance=temp_distance;
+			  nearest=i;
 			  }
-			  
-		  return true;		  
+			  else
+			  {
+				  if(distance > temp_distance)
+					  distance=temp_distance;
+				      nearest=i;
+			  }
 		  }
-		
-	}
-  return false;
-  }
+		 
+	  }
+	  
+	  if(nearest==-1)
+		  return false;
+	  else if (nearest==-2)
+	  {
+		  super.shipItem(name,amount);
+		  return true;
+	  }
+	  else
+	  {
+		  hubs.get(nearest).shipItem(name,amount);
+		  return true;
+	  }
   
 }
