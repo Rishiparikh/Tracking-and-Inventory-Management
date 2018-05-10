f4ff013792e2ec142b85dda58da550f85cdc35bf
rkolli0603 <38920181+rkolli0603@users.noreply.github.com>
2018-05-09 21:21:09 -0700

    Update Item.java

--- Item.java
+++ Item.java
@@ -1,34 +1,65 @@
+  /**
+   * Item class
+   * Contains the items to be shipped
+   */
 public class Item {
   private String name;
   private int quantity;
-  
+   /**
+   * Constructor
+   * @param name1 = name of an item
+   * @param quant = quantity of a specific item
+   */
   public Item(String name1, int quant){
     name = name1;
     quantity = quant;
   }
+  /**
+   * compare to method
+   * Compares the items
+   * @param a = object of type Item to be compared
+   * @return the compared item
+   */
   public int compareTo(Item a){
     return name.compareTo(a.name);
   }
+   /**
+   * Getter MEthod to get the quantity of an item
+   * @return the quantity of the item
+   */
   public int getQuantity(){
     return quantity;
   }
+ /**
+   * Setter method to set the quantity of a particular item
+   * @param a = number that will be set to the quantity of the item
+   */
   public void setQuantity(int a) {
 	  
 	  if(a > -1)
 	  quantity = a;
   }
-  
-  public void updateQuantity(int a) {
-	  
-		 
+   /**
+   * Method to update the quantity of an item after being shipped to a location
+   * @param a = the quantity number to be updated
+   */
+  public void updateQuantity(int a) {	 
 	  quantity = quantity-a;
   } 
+  /**
+   * Method to refill an item with a quantity
+   * @param a = the quantity to be refilled
+   */
   public void refill(int a) {
 	  
 	 if(a > 0)
 	  quantity = quantity+a;
   }
- 
+  /**
+   * Method to check the availability of an item
+   * @param amount = check the amount of an item we have
+   * @return wether we have space
+   */
  
  public boolean checkAvailability(int amount){
 	    if ( amount > quantity)
@@ -36,6 +67,10 @@ public class Item {
 	    	else
 	    	 return true;
 	  }
+ /**
+   * Getter method for the name of an item
+   * @return the name of an item
+   */
   public String getName() {
 	  return name;
   }
