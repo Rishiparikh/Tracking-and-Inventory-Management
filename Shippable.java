223a636355e15c5734213efd5b2378d423b97dab
Rishi Parikh <rishiparikh2001@gmail.com>
2018-05-12 14:47:18 -0700

    Update Shippable.java

--- Shippable.java
+++ Shippable.java
@@ -5,7 +5,6 @@ import java.util.ArrayList;
    */
 public interface Shippable{
   public int[] getLocation();
-  //public ArrayList<Item> getInv;
   public void addInv(Item a);
   public boolean hasUpdate();
   public int checkYellowAlert();
