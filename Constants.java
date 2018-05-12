0b8baa63aae6a4ff025eddc12265d83e31989c69
Rishi Parikh <rishiparikh2001@gmail.com>
2018-05-12 15:53:26 -0700

    Update Constants.java

--- Constants.java
+++ Constants.java
@@ -3,9 +3,16 @@
    * These Values will never change
    */
 public class Constants{
+  static final int[] loc0 = {150, 150};
+  static final int[] loc1 = {800, 200};
+  static final int[] loc2 = {450, 250};
+  static final int[] loc3 = {600, 450};
+  static final int[] loc4 = {300, 400};
   static final int smallStorageMax = 100;
   static final int largeStorageMax = 500;
   static final int redAlert = 25;
-  static final int yellowAlert = 45;
+  static final int yellowAlert = 50;
   static final int refillQuantity=10;
+  static final int redAlertLarge = 40;
+  static final int yellowAlertLarge = 140;
 }
