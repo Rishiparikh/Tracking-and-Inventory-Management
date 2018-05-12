f56e9beab72597d9b4ac7e455c17d9d9dad984ed
rkolli0603 <38920181+rkolli0603@users.noreply.github.com>
2018-05-12 13:24:33 -0700

    Update Constants.java

--- Constants.java
+++ Constants.java
@@ -1,16 +1,11 @@
  /**
    * Class that contains all the constants
-   * 
+   * These Values will never change
    */
 public class Constants{
   static final int smallStorageMax = 100;
-  static final int[] locationA1 = {100,100};
-  static final int[] locationA2 = {200,100};
-  static final int[] locationA3 = {300,100};
-  static final int[] locationA4 = {400,100};
-  static final int largeStorageMax = 300;
-  static final int[] wareHouseLocation = {0,0};
+  static final int largeStorageMax = 500;
   static final int redAlert = 25;
-  static final int yellowAlert = 33;
-  static final int refillQuantity=4;
+  static final int yellowAlert = 45;
+  static final int refillQuantity=10;
 }
