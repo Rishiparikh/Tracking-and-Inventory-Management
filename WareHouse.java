89616277345f427e070d5dcd1418623eb4e1b97e
rkolli0603 <38920181+rkolli0603@users.noreply.github.com>
2018-05-12 14:39:41 -0700

    Update WareHouse.java

--- WareHouse.java
+++ WareHouse.java
@@ -1,116 +1,93 @@
-/**
- * @author Shaheryar Asad
- * Period: 3
- * 
- */
 import java.util.ArrayList;
+/**
+ * Warehouse class
+ *
+ */
+public class WareHouse extends Storage implements Shippable {
+	private ArrayList<Storage> hubs = new ArrayList<Storage>();
+	private final int refillQuantity;
+	/**
+	 * Constructor for Warehouse class
+	 * @param a - array list of storage centers
+	 * @param b = array list of items
+	 * @param xCord = x coordinate of item
+	 * @param yCord = y coordinate of item
+	 * @param red = color for when storage is in need of immediate refill
+	 * @param yellow = color when storage needs to be refilled
+	 * @param max = max amount an item can contain
+	 */
+	public WareHouse(ArrayList<Storage> a, ArrayList<Item> b, int xCord, int yCord, int red, int yellow, int max) {
+		super(b, xCord, yCord, red, yellow, max);
+		hubs = a;
+		refillQuantity = 10;
+	}
+	/**
+	 * Checks for updates
+	 * @return true if item has an update
+	 */
+	public boolean hasUpdate() {
+		for (int i = 0; i < hubs.size(); i++) {
+			hubs.get(i).checkRedAlert();
+			hubs.get(i).checkYellowAlert();
+			if (hubs.get(i).hasUpdate()) {
+				return true;
+			}
+		}
+		return false;
+	}
+	/**
+	 * Used to add a location to our storage
+	 * @param a = storage 
+	 */
+	public void addLocation(Storage a) {
+		hubs.add(a);
+	}
 
-public class WareHouse extends Storage implements Shippable{
-	  private ArrayList<Storage> hubs = new ArrayList<Storage>();
-    
-  
-  public WareHouse(ArrayList<Storage> ports, ArrayList<Item> wareHouseItems, int xCord, int yCord, int red, int yellow){
-    super(wareHouseItems, xCord, yCord, red, yellow, Constants.largeStorageMax);
-    //ArrayList <Storage> mainInventory = new ArrayList<Storage>();
-    hubs = ports; 
-  }
-  
-  public boolean hasUpdate(){
-    for(int i = 0; i < hubs.size(); i++){
-      if(hubs.get(i).hasUpdate()){
-        return true;
-      }
-    }
-    return false;
-  }
-  
-  public void refillHub(int i)
-  {
-	  if ((i >= 0) && (i< hubs.size())){
-		  int flag=hubs.get(i).checkRedAlert();
-		  String itemName;
-		  if(flag>=0)
-		  {
-			  itemName= hubs.get(i).getItemName(flag);
-			 hubs.get(i).refilItem(itemName, Constants.refillQuantity);
-			 super.shipItem(itemName, Constants.refillQuantity);
-		  }
-		  
-		  flag=hubs.get(i).checkYellowAlert();
-		  if(flag>=0)
-		  {
-			  itemName= hubs.get(i).getItemName(flag);
-			 hubs.get(i).refilItem(itemName, Constants.refillQuantity);
-			 super.shipItem(itemName, Constants.refillQuantity);
-		  }
-	  }
-  }
-  // returns : 0 for normal status, 1 : for yellow flag and 2 : for red flag
-  public int hasUpdate(int i)
-  {
-	  if ((i >= 0) && (i< hubs.size())) {  
-		  if(hubs.get(i).checkRedAlert() >= 0 )
-		    	return 2;
-		    else if(hubs.get(i).checkYellowAlert() >= 0 )
-		    	return 1;
-		    else 
-		    	return 0;
-	  }
-	  return 0;
-  }
+	public void getStatus() {
 
-  public boolean shipItem(String name, int amount, int xCord, int yCord)
-  {
-	  int nearest = -1;
-	  int distance=-1;
-	  
-	  // first check if the warehouse has the item stored and what is the distance
-	  
-	  int [] wareLoc=super.getLocation();
-	  int wareHouse_distance = (wareLoc[0]-xCord)* (wareLoc[0]-xCord) + (wareLoc[1]-yCord)* (wareLoc[1]-yCord);
-	  if(super.hasItemQuantity(name,amount))
-	  {
-		  nearest=-2;
-		  distance=wareHouse_distance;
-	  }
-	  for (int i = 0; i < hubs.size(); i++) 
-	  {
-		  int [] loc = hubs.get(i).getLocation();
-		  int temp_distance = (loc[0]-xCord)* (loc[0]-xCord) + (loc[1]-yCord)* (loc[1]-yCord);
-		  if( hubs.get(i).hasItemQuantity(name,amount))
-		  {
-			  if(nearest==-1)
-			  {
-			  distance=temp_distance;
-			  nearest=i;
-			  }
-			  else
-			  {
-				  if(distance > temp_distance)
-				  {
-					  distance=temp_distance;
-				      nearest=i;
-				  }
-			  }
-		  }
-	  }
-	  
-	  if(nearest==-1)
-		  return false;
-	  else if (nearest==-2)
-	  {
-		  super.shipItem(name,amount);
-		  System.out.println("shiping Item from : "+ "WareHouse");
+	}
 
-		  return true;
-	  }
-	  else
-	  {
-		  hubs.get(nearest).shipItem(name,amount);
-		  
-		  System.out.println("shiping Item from : "+ nearest);
-		  
-		  return true;
-	  }
-  }
+	@Override
+	/**
+	 * Statement method to print out the contents of an item
+	 * @return the contents of an item
+	 */
+	public String statement() {
+		String temp = "Warehouse: ";
+		for (int i = 0; i < myInv().size(); i++) {
+			temp += myInv().get(i).getName() + " ";
+			temp += myInv().get(i).getQuantity() + " \n";
+		}
+		return temp;
+	}
+	/**
+	 * List to store all the hubs
+	 * @return the hubs
+	 */
+	public ArrayList<Storage> hubs() {
+		return hubs;
+	}
+	/**
+	 * Method to refill a hub
+	 * @param i = amount to refill
+	 */
+	public void refillHub(int i) {
+		if ((i >= 0) && (i < hubs.size())) {
+			int flag = hubs.get(i).checkRedAlert();
+			String itemName;
+			if (flag >= 0) {
+				itemName = hubs.get(i).getItemName(flag);
+				hubs.get(i).refilItem(itemName, refillQuantity);
+				super.shipItem(itemName, refillQuantity);
+			}
+
+			flag = hubs.get(i).checkYellowAlert();
+			if (flag >= 0) {
+				itemName = hubs.get(i).getItemName(flag);
+				hubs.get(i).refilItem(itemName, refillQuantity);
+				super.shipItem(itemName, refillQuantity);
+			}
+		}
+		hubs.get(i).checkMax();
+	}
 }
