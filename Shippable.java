2d10ecb703a000e82e3a584e3f494dc4d03dcb54
Rishi Parikh <rishiparikh2001@gmail.com>
2018-05-12 20:11:03 -0700

    Update Shippable.java

--- Shippable.java
+++ Shippable.java
@@ -1,13 +1,19 @@
 import java.util.ArrayList;
- /**
-   * Shippable Interface
-   * Contains outilne for Warehouse class
-   */
-public interface Shippable{
-  public int x();
-  public int y();
-  public void addInv(Item a);
-  public boolean hasUpdate();
-  public int checkYellowAlert();
-  public int checkRedAlert();
+
+/**
+ * @author Rahul Kolli
+ * Shippable Interface Contains outilne for Warehouse class
+ */
+public interface Shippable {
+	public int x();
+
+	public int y();
+
+	public void addInv(Item a);
+
+	public boolean hasUpdate();
+
+	public int checkYellowAlert();
+
+	public int checkRedAlert();
 }
